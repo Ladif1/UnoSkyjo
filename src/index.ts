3f96c0d6f90d9f93b021656cef82381996434a8b
Raaaaphh <raphmangini@gmail.com>
2024-03-19 10:50:52 +0100

    Creation des classes cartes

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ async function main() {
 }
 
 
+
 main();
---
+++ src/models/uno.ts
@@ -0,0 +1,37 @@
+class Uno extends Card
+{
+
+    constructor(id: number, value: string, couleur:string, point: number) {
+        super(id, value, couleur, point);
+    }
+
+    
+    // Getters
+    getId(): number {
+        return this.id;
+    }
+    getValue(): string {
+        return this.value;
+    }
+    getCouleur(): string {
+        return this.couleur;
+    }
+    getPoint(): number {
+        return this.point;
+    }
+
+
+    // Setters
+    setId(id: number): void {
+        this.id = id;
+    }
+    setValue(value: string): void {
+        this.value = value;
+    }
+    setCouleur(couleur: string): void {
+        this.couleur = couleur;
+    }
+    setPoint(point: number): void {
+        this.point = point;
+    }
+}
